3cc5d1d5593d0efcf84faad4bc5511e6b8f5b59c
Ishan Saini <ishansaini0105@gmail.com>
2025-03-08 18:01:12 +0530

    Sidebar and song player section is completed in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Sidebar from './components/sidebar'
+import Player from './components/player'
+
 const App = () => {
   return (
     <div className=' w-screen h-screen bg-black'>
       <div className='h-[90%] flex'>
     <Sidebar/>
       </div>
+      <Player/>
     </div>
   )
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,19 +4,19 @@ const sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
 
-        <div className=' border-1 border-white bg-blue-600 h-[15%] rounded flex flex-col justify-around'>
+        <div className=' border-1 border-white bg-blue-700 h-[15%] rounded flex flex-col justify-around'>
 
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
 
 <img className='w-6' src={assets.home_icon} alt="loading" />
-<p className='font-bold'>Home</p>
+<p className='font-bold hover:text-slate-700 duration-300'>Home</p>
             </div>
 
 
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
 
 <img className='w-6' src={assets.search_icon} alt="loading" />
-<p className='font-bold'>Search</p>
+<p className='font-bold hover:text-slate-700 duration-300'>Search</p>
             </div>
 
 
@@ -29,30 +29,30 @@ const sidebar = () => {
     <div className='p-4 flex items-center justify-between'>
         <div className='fle items-center gap-3'>
             <img className='w-8' src={assets.stack_icon} alt="" />
-            <p className='font-semibold'>Your Collection</p>
+            <p className='font-semibold '>Your Collection</p>
 
         </div>
 
 <div className='flex items-center gap-3'>
-<img className='w-5' src={assets.arrow_icon} alt="" />
-<img className='w-5' src={assets.plus_icon} alt="" />
+<img className='w-5 text-black' src={assets.arrow_icon} alt="" />
+<img className='w-5 text-black' src={assets.plus_icon} alt="" />
 </div>
 
     </div>
 
-<div className=' border-1 border-white p-4 bg-blue-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
+<div className=' border-1 border-white p-4 bg-black m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
 
 <h1>Create Your First Collection</h1>
-<p className='font-light'>Let us help you in this.</p>
-<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Collection</button>
+<p className='font-light text-white '>Let us help you in this.</p>
+<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 border-1 border-green-500 hover:bg-gray-300 duration-300'>Create Collection</button>
 </div>
 
 
-<div className=' border-1 border-white p-4 bg-blue-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
+<div className=' border-1 border-white p-4 bg-black m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
 
 <h1>Check the latest Podcastes</h1>
-<p className='font-light'>Let us inform you in this.</p>
-<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcastes</button>
+<p className='font-light text-white'>Let us inform you in this.</p>
+<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 border-1 border-green-500 hover:bg-gray-300 duration-300'>Browse Podcastes</button>
 </div>
 
 
