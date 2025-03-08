79a690dfb3c93eccfe71ff3d3ece7847b9600826
Ishan Saini <ishansaini0105@gmail.com>
2025-03-08 17:21:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/sidebar'
+const App = () => {
+  return (
+    <div className=' w-screen h-screen bg-black'>
+      <div className='h-[90%] flex'>
+    <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {assets} from '../assets/frontend-assets/assets'
+const sidebar = () => {
+  return (
+    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+
+        <div className=' border-1 border-white bg-blue-600 h-[15%] rounded flex flex-col justify-around'>
+
+            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+
+<img className='w-6' src={assets.home_icon} alt="loading" />
+<p className='font-bold'>Home</p>
+            </div>
+
+
+            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+
+<img className='w-6' src={assets.search_icon} alt="loading" />
+<p className='font-bold'>Search</p>
+            </div>
+
+
+
+
+        </div>
+      
+
+<div className='border-1 border-white bg-blue-600 h-[100%] rounded'>
+    <div className='p-4 flex items-center justify-between'>
+        <div className='fle items-center gap-3'>
+            <img className='w-8' src={assets.stack_icon} alt="" />
+            <p className='font-semibold'>Your Collection</p>
+
+        </div>
+
+<div className='flex items-center gap-3'>
+<img className='w-5' src={assets.arrow_icon} alt="" />
+<img className='w-5' src={assets.plus_icon} alt="" />
+</div>
+
+    </div>
+
+<div className=' border-1 border-white p-4 bg-blue-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
+
+<h1>Create Your First Collection</h1>
+<p className='font-light'>Let us help you in this.</p>
+<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Collection</button>
+</div>
+
+
+<div className=' border-1 border-white p-4 bg-blue-700 m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 '>
+
+<h1>Check the latest Podcastes</h1>
+<p className='font-light'>Let us inform you in this.</p>
+<button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcastes</button>
+</div>
+
+
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default sidebar
