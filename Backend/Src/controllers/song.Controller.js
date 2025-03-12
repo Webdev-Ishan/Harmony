a9a4ad796d93c57c86027b496b2556dbc6ad88df
Ishan Saini <ishansaini0105@gmail.com>
2025-03-12 10:41:02 +0530

    The Add song controller function is created

--- Backend/Src/controllers/song.Controller.js
+++ Backend/Src/controllers/song.Controller.js
@@ -1,11 +1,46 @@
-const Addsong = (req,res)=>{
+import { v2 as cloudinary } from "cloudinary";
+import songModel from "../models/song.Model.js";
 
+const Addsong = async (req, res) => {
+  try {
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const album = req.body.album;
+    const audio = req.files.audio[0];
+    const image = req.files.image[0];
 
-}
+    const audioUpload = await cloudinary.uploader.upload(audio.path, {
+      resource_type: "video",
+    });
+    const imageUpload = await cloudinary.uploader.upload(image.path, {
+      resource_type: "image",
+    });
 
-const Listsong = (req,res)=>{
+    const duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(
+      audioUpload.duration % 60
+    )}`;
 
+    const songdata = {
+      name,
+      desc,
+      album,
+      image: imageUpload.secure_url,
+      audio: audioUpload.secure_url,
+      duration,
+    };
 
-}
+    const song = new songModel( songdata );
+    await song.save();
 
-export {Addsong,Listsong}
+    res.json({ success: true, message: "Song Added" });
+
+  }
+   catch (error) {
+console.log(error)
+    res.json({ success: false });
+  }
+};
+
+const Listsong = (req, res) => {};
+
+export { Addsong, Listsong };
--- Backend/Src/routes/songRoute.js
+++ Backend/Src/routes/songRoute.js
@@ -1,9 +1,10 @@
 import { Addsong,Listsong } from "../controllers/song.Controller.js";
 import  express from 'express'
+import upload from "../middleware/multer.js";
 
 const songRouter = express.Router()
 
-songRouter.post('/add',Addsong)
+songRouter.post('/add',upload.fields([{name:'image',maxCount:1},{name:"audio",maxCount:1}]),Addsong)
 songRouter.get('/list',Listsong)
 
 
