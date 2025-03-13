7d32c427001a946461ba98e6b09f50f7a84500ee
Ishan Saini <ishansaini0105@gmail.com>
2025-03-13 11:48:36 +0530

    The addsong page is created

--- Harmony_Admin/src/App.jsx
+++ Harmony_Admin/src/App.jsx
@@ -6,14 +6,16 @@ import Addalbum from './pages/Addalbum'
 import Listsong from './pages/Listsong'
 import Listalbum from './pages/Listalbum'
 import Sidebar from './components/sidebar'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer/>
-      
+
 <Sidebar/>
       <div className='flex-1 h-screen overflow-y-scroll bg-white'>
+        <Navbar/>
 <div className='pt-8 pl-5 sm:pt-15 sm:pl-12'>
   <Routes>
 
--- Harmony_Admin/src/pages/Addsong.jsx
+++ Harmony_Admin/src/pages/Addsong.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { assets } from "../assets/admin-assets/assets";
 const Addsong = () => {
+
+
+const [image,setimage] = useState(flase)
+const [song,setsong] = useState(flase)
+const [name,setname] = useState("")
+const [desc,setdesc] = useState("")
+const [album,setalbum] = useState("none")
+const [loading,setloading] = useState(flase)
+const [albumdata,setalbumdata] = useState([])
+
   return (
     <div>
-      h1
-    </div>
-  )
-}
+      <form action="" className="flex flex-col items-start gap-8 text-black ">
+        <div className="flex gap-8">
+          <div className="flex flex-col gap-4">
+            <p>Upload Song</p>
+            <input type="file" id="song" accept="audio/*" hidden />
+            <label htmlFor="song">
+              <img
+                src={assets.upload_song}
+                alt=""
+                className="w-24 cursor-pointer"
+              />
+            </label>
+          </div>
 
-export default Addsong
+<div className="flex flex-col gap-4">
+<p>Upload Image</p>
+<input type="file" id="image" accept="image/*" hidden />
+<label htmlFor="image">
+              <img
+                src={assets.upload_area}
+                alt=""
+                className="w-24 cursor-pointer"
+              />
+            </label>
+</div>
+
+        </div>
+
+
+<div className="flex flex-col gap-2.5">
+  <p>Song Name</p>
+  <input className="bg-transparent outline-green-500 border-2 border-black p-2 w-[max(40vw,250px)] " placeholder="Type here" required type="text" />
+
+
+</div>
+
+
+<div className="flex flex-col gap-2.5">
+  <p>Song Description</p>
+  <input className="bg-transparent outline-green-500 border-2 border-black p-2 w-[max(40vw,250px)] " placeholder="Type here" required type="text" />
+
+
+</div>
+
+
+<div className="flex flex-col gap-2.5">
+<p>Album</p>
+<select className="bg-transparent outline-green-500 border-2 border-black p-2 w-[max(10vw,25px)]">
+<option value="none">None</option>
+</select>
+
+</div>
+
+<button type="submit" className="border-black border-2 text-white bg-gradient-to-r from-violet-700 to-blue-700 font-medium w-24 text-md px-4 py-2 mb-2 hover:opacity-75 ">Add</button>
+
+      </form>
+    </div>
+  );
+};
+
+export default Addsong;
