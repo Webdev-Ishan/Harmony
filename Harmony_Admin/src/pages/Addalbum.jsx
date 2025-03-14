bebe03b726e4c2946e2e500dac8bf416a75310b7
Ishan Saini <ishansaini0105@gmail.com>
2025-03-14 16:29:55 +0530

    The addAlbum functionality is built

--- Harmony_Admin/src/pages/Addalbum.jsx
+++ Harmony_Admin/src/pages/Addalbum.jsx
@@ -1,10 +1,116 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { assets } from '../assets/admin-assets/assets'
+import axios from 'axios'
+import { toast } from "react-toastify";
+import { url } from "../App";
+
 
 const Addalbum = () => {
-  return (
-    <div>
-      h2
+
+const [image, setImage] = useState(null);
+  const [colour, setcolour] = useState("");
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [loading, setLoading] = useState(false);
+  
+
+
+const onSubmithandler = async (e)=>{
+
+e.preventDefault();
+setLoading(true)
+
+
+try {
+  
+
+  const formData = new FormData();
+
+  formData.append("name", name);
+  formData.append("desc", desc);
+  formData.append("bgColour", colour);
+  formData.append("image", image);
+
+  const response = await axios.post(`${url}/api/album/add`,formData);
+
+
+      if (response.data.success) {
+        toast.success("Song Added");
+        setName("");
+        setDesc("");
+        setImage(false);
+        setcolour(null)
+
+      } else {
+        toast.error("Something went wrong");
+      }
+    } catch (error) {
+
+
+      toast.error("Error occurred");
+      console.log(error.message);
+    }
+    setLoading(false);
+
+
+};
+
+  return loading ? (
+    <div className="flex items-center justify-center h-screen">
+      <p>Loading...</p>
     </div>
+  ) : (
+    <form onSubmit={onSubmithandler} action="" className='flex flex-col items-start gap-8 text-gray-600'>
+
+<div className='flex flex-col gap-4'>
+<p>Upload Image</p>
+<input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image'  accept="image/*"
+ hidden />
+
+<label htmlFor="image">
+
+  <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+</label>
+</div>
+
+
+<div className="flex flex-col gap-2.5">
+        <p>Album Name</p>
+        <input
+        onChange={(e)=>setName(e.target.value)}
+        value={name}
+          className="bg-transparent outline-green-500 border-2 border-black p-2 w-[max(40vw,250px)]"
+          placeholder="Type here"
+          required
+          type="text"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2.5">
+        <p>Album Description</p>
+        <input
+        onChange={(e)=>setDesc(e.target.value)} value={desc}
+          className="bg-transparent outline-green-500 border-2 border-black p-2 w-[max(40vw,250px)]"
+          placeholder="Type here"
+          required
+          type="text"
+        />
+      </div>
+
+<div className='flex flex-col gap-3'>
+<p>Background Colour</p>
+<input onChange={(e)=>setcolour(e.target.value)} value={colour} type="color" />
+
+</div>
+
+<button
+        type="submit"
+        className="border-black border-2 text-white bg-gradient-to-r from-violet-700 to-blue-700 font-medium w-24 text-md px-4 py-2 mb-2 hover:opacity-75"
+      >
+        Add
+      </button>
+
+    </form>
   )
 }
 
--- Harmony_Admin/src/pages/Listsong.jsx
+++ Harmony_Admin/src/pages/Listsong.jsx
@@ -9,9 +9,9 @@ const Listsong = () => {
   const fetchSongs = async () => {
     try {
       const response = await axios.get(`${url}/api/song/list`);
-      
+
       if (response.data.success) {
-        console.log(response.data.song)
+        console.log(response.data.song);
         setData(response.data.song);
       } else {
         toast.error("Something went wrong");
@@ -22,52 +22,47 @@ const Listsong = () => {
     }
   };
 
+  const removesong = async (id) => {
+    try {
+      const response = await axios.post(`${url}/api/song/remove`, { id });
 
-const removesong = async (id)=>{
-
-try {
-
-  const response = await axios.post(`${url}/api/song/remove`,{id})
-
-  if(response.data.success){
-
-toast.success(response.data.message)
-await fetchSongs()
-
-  }
-} catch (error) {
-  
-  toast.error("Something went wrong")
-}
-
-}
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchSongs();
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
 
   useEffect(() => {
     fetchSongs();
   }, []);
 
   return (
-    <div>
-      <p>All Songs List</p>
-      <br />
-      <div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">All Songs List</h1>
+      <div className="overflow-x-auto">
         <table className="table-auto w-full bg-blue-500 border-2 border-black">
           <thead>
             <tr>
-              <th className="px-4 py-2">Image</th>
-              <th className="px-4 py-2">Name</th>
-              <th className="px-4 py-2">Album</th>
-              <th className="px-4 py-2">Duration</th>
-              <th className="px-4 py-2">Action</th>
+              <th className="px-4 py-2 text-white">Image</th>
+              <th className="px-4 py-2 text-white">Name</th>
+              <th className="px-4 py-2 text-white">Album</th>
+              <th className="px-4 py-2 text-white">Duration</th>
+              <th className="px-4 py-2 text-white">Action</th>
             </tr>
           </thead>
 
           <tbody>
-
             {data.map((song) => (
-              <tr key={song.id}>
+              <tr key={song.id} className="bg-blue-700 hover:bg-blue-600">
                 <td className="border px-4 py-2 border-white">
-                  <img src={song.image} alt={song.name} className="w-16 h-16  text-white" />
+                  <img
+                    src={song.image}
+                    alt={song.name}
+                    className="w-16 h-16"
+                  />
                 </td>
                 <td className="border px-4 py-2 text-white">{song.name}</td>
                 <td className="border px-4 py-2 text-white">{song.album}</td>
@@ -75,13 +70,13 @@ await fetchSongs()
                 <td className="border px-4 py-2 text-white">
                   <button
                     onClick={() => removesong(song._id)}
-                    className="bg-red-500 text-white px-4 py-2 rounded  cursor-pointer"
+                    className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-red-700"
                   >
                     Delete
                   </button>
                 </td>
               </tr>
-))}
+            ))}
           </tbody>
         </table>
       </div>
