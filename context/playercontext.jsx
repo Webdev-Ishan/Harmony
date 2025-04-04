ec7f14f2bca88eacd554cfed1cb884773ef27e6b
Ishan Saini <ishansaini0105@gmail.com>
2025-04-04 17:17:12 +0530

    Update playercontext.jsx

--- context/playercontext.jsx
+++ context/playercontext.jsx
@@ -8,7 +8,7 @@ const PlayercontextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = 'http://localhost:4000';
+  const url = 'https://harmony-backend-26ed.onrender.com';
 
   const [songsData, setSongsData] = useState([]);
   const [albumData, setAlbumData] = useState([]);
